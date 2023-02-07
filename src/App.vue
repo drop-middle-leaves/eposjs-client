7731b9c241a6c222be8e48cbfce0ec61d5996523
Els Allaway <66450509+eloiseaa728@users.noreply.github.com>
2023-02-07 12:42:43 +0000

    splitting git repos to make my life easier

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+  <body>
+  <div id="app">
+    <div class="container-fluid" style="height: 100%;">
+      <div class="row h-50 mt-4">
+        <div class="col-6">
+          <div class="box w-100"></div>
+        </div>
+        <div class="col-6">
+          <div class="box w-100"></div>
+        </div>
+      </div>
+      <div class="row mt-4 h-50">
+        <div class="col-6">
+          <div class="box w-100"></div>
+        </div>
+        <div class="col-6">
+          <div class="box w-100"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </body>
+</template>
+
+<style scoped>
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Importing vue functions
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Importing bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+// Creating the app and mounting the div id app from App.vue
+createApp(App).mount('#app')
