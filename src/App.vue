141d60273443ebea518dc5e888b753fde94e4863
Els Allaway <66450509+eloiseaa728@users.noreply.github.com>
2023-02-11 15:42:05 +0000

    Change how the margin works; split the searchTotal.vue file.

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,47 @@
 <script setup>
-import CurrentTable from "@/components/currentTable.vue";
-import SearchTotal from "@/components/searchTotal.vue";
+import currentTable from "@/components/currentTable.vue";
+import search from "@/components/search.vue";
+import searchButton from "@/components/search-button.vue";
+import total from "@/components/total.vue";
 </script>
 
 <template>
-  <!-- mounts to main.js, centralises the container containing the grid to allow for container -->
-  <div id="app" class="flex h-screen w-screen justify-center">
-    <!-- creates the outside container, creating the outside margin -->
-    <div class="h-[calc(100vh_-_1rem)] w-[calc(100vw_-_1rem)] self-center">
-      <!-- creates the 1st row -->
-      <div class="flex-row flex h-1/2 w-full">
-        <!-- creates the 1st column of the first row -->
-        <div class="flex-col h-full w-full">
-          <current-table />
+  <!-- mounts to main.js, makes the size the size of the screen -->
+  <div id="app" class="h-screen w-screen">
+    <!-- creates the 1st row -->
+    <div class="flex-row flex h-1/2 w-full">
+      <!-- creates the 1st column of the first row -->
+      <div class="flex-col h-full w-full">
+        <current-table />
+      </div>
+      <!-- creates the 2nd column of the first row -->
+      <div class="flex-col flex h-full w-full">
+        <!-- creates the area for the search box (first row, second column -> first row) -->
+        <div class="flex-row h-1/2 w-full flex justify-center">
+          <search />
         </div>
-        <!-- creates the 2nd column of the first row -->
-        <div class="flex-col flex h-full w-full">
-          <!-- creates the area for the search box (first row, second column -> first row) -->
-          <div class="flex-row h-1/2 w-full bg-cyan-500">yo</div>
-          <!-- creates a second row (first row, second column -> second row) -->
-          <div class="flex-row flex h-1/2 w-full">
-            <!-- creates the first column ^ -->
-            <div class="flex-col h-full w-1/2 bg-amber-500">whats up</div>
-            <!-- creates the second column ^ -->
-            <div class="flex-col h-full w-1/2 bg-gray-500">fuzz nut</div>
+        <!-- creates a second row (first row, second column -> second row) -->
+        <div class="flex-row flex h-1/2 w-full">
+          <!-- creates the first column ^ -->
+          <div class="flex-col h-full w-1/2 flex justify-center">
+            <total />
+          </div>
+          <!-- creates the second column ^ -->
+          <div class="flex-col h-full w-1/2 flex justify-center">
+            <search-button />
           </div>
         </div>
       </div>
-      <!-- creates the second row -->
-      <div class="flex-row flex h-1/2 w-full">
-        <!-- creates the first column of the second row -->
-        <div class="flex-col h-full w-full">
-          <div class="bg-green-500 h-full w-full"></div>
-        </div>
-        <!-- creates the second column of the second row -->
-        <div class="flex-col h-full w-full">
-          <div class="bg-red-500 h-full w-full"></div>
-        </div>
+    </div>
+    <!-- creates the second row -->
+    <div class="flex-row flex h-1/2 w-full">
+      <!-- creates the first column of the second row -->
+      <div class="flex-col h-full w-full">
+        <div class="bg-green-500 h-full w-full"></div>
+      </div>
+      <!-- creates the second column of the second row -->
+      <div class="flex-col h-full w-full">
+        <div class="bg-red-500 h-full w-full"></div>
       </div>
     </div>
   </div>
--- src/components/currentTable.vue
+++ src/components/currentTable.vue
@@ -2,25 +2,27 @@
 </script>
 
 <template>
-  <div class="h-[calc(100%_-_0.5rem)] w-[calc(100%_-_0.5rem)] border overflow-y-scroll scrollbar scrollbar-thumb-rounded-lg scrollbar-thumb-gray-300 rounded-lg">
-    <table class="table-auto w-full">
-      <thead class="bg-gray-500">
-        <tr>
-          <th class="px-4 py-2">Item</th>
-          <th class="px-4 py-2">Qty</th>
-          <th class="px-4 py-2">Price</th>
-          <th class="px-4 py-2">Total</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="i in 20">
-          <th class="border px-4 py-2">Irn Bru {{ i }}</th>
-          <td class="border px-4 py-2">1</td>
-          <td class="border px-4 py-2">1.00</td>
-          <td class="border px-4 py-2">1.00</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="h-full w-full flex justify-center">
+    <div class="h-[calc(100%_-_1rem)] w-[calc(100%_-_1rem)] border overflow-y-scroll scrollbar scrollbar-thumb-rounded-lg scrollbar-thumb-gray-300 rounded-lg self-center">
+      <table class="table-auto w-full">
+        <thead class="bg-gray-500">
+          <tr>
+            <th class="px-4 py-2">Item</th>
+            <th class="px-4 py-2">Qty</th>
+            <th class="px-4 py-2">Price</th>
+            <th class="px-4 py-2">Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="i in 20">
+            <th class="border px-4 py-2">Irn Bru {{ i }}</th>
+            <td class="border px-4 py-2">1</td>
+            <td class="border px-4 py-2">1.00</td>
+            <td class="border px-4 py-2">1.00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 
 </template>
