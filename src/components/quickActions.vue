a8989d8d85c569bf657432f2ec0031c864f721f8
Els Allaway <66450509+eloiseaa728@users.noreply.github.com>
2023-02-20 00:12:04 +0000

    eslint + rearranging (goodnight)

--- src/components/quickActions.vue
+++ src/components/quickActions.vue
@@ -1,42 +1,73 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <div class="h-full w-full flex justify-center">
+  <div class="flex justify-center w-full h-full">
     <!-- Creates outer border -->
-    <div class="h-[calc(100%_-_1rem)] w-[calc(100%_-_1rem)] rounded-lg self-center">
+    <div
+      class="h-[calc(100%_-_1rem)] w-[calc(100%_-_1rem)] rounded-lg self-center"
+    >
       <!-- Creates grid -->
-      <div class="grid grid-rows-3 grid-flow-col gap-2 h-full w-full flex">
+      <div class="grid w-full h-full grid-flow-col grid-rows-3 gap-2">
         <!-- col 1 -->
-        <button type="button" class="bg-orange-500 hover:bg-orange-600 buttonWrapper">
+        <button
+          type="button"
+          class="bg-orange-500 hover:bg-orange-600 buttonWrapper"
+        >
           <font-awesome-icon icon="fa-solid fa-hammer" class="buttonIcon" />
         </button>
-        <button type="button" class="bg-cyan-500 hover:bg-cyan-600 buttonWrapper">
-          <font-awesome-icon icon="fa-solid fa-shopping-bag" class="buttonIcon" />
+        <button
+          type="button"
+          class="bg-cyan-500 hover:bg-cyan-600 buttonWrapper"
+        >
+          <font-awesome-icon
+            icon="fa-solid fa-shopping-bag"
+            class="buttonIcon"
+          />
         </button>
-        <button type="button" class="bg-orange-500 hover:bg-orange-600 buttonWrapper">
-          <font-awesome-icon icon="fa-solid fa-arrow-rotate-left" class="buttonIcon" />
+        <button
+          type="button"
+          class="bg-orange-500 hover:bg-orange-600 buttonWrapper"
+        >
+          <font-awesome-icon
+            icon="fa-solid fa-arrow-rotate-left"
+            class="buttonIcon"
+          />
         </button>
 
         <!-- col 2 -->
         <button type="button" class="bg-red-500 hover:bg-red-600 buttonWrapper">
           <font-awesome-icon icon="fa-solid fa-xmark" class="buttonIcon" />
         </button>
-        <button type="button" class="bg-cyan-500 hover:bg-cyan-600 buttonWrapper">
+        <button
+          type="button"
+          class="bg-cyan-500 hover:bg-cyan-600 buttonWrapper"
+        >
           <font-awesome-icon icon="fa-solid fa-bars" class="buttonIcon" />
         </button>
         <button type="button" class="bg-red-500 hover:bg-red-600 buttonWrapper">
-          <font-awesome-icon icon="fa-solid fa-xmarks-lines" class="buttonIcon" />
+          <font-awesome-icon
+            icon="fa-solid fa-xmarks-lines"
+            class="buttonIcon"
+          />
         </button>
 
         <!-- col 3 -->
-        <button type="button" class="bg-orange-500 hover:bg-orange-600 buttonWrapper">
+        <button
+          type="button"
+          class="bg-orange-500 hover:bg-orange-600 buttonWrapper"
+        >
           <font-awesome-icon icon="fa-solid fa-money-bill" class="buttonIcon" />
         </button>
-        <button type="button" class="bg-yellow-500 hover:bg-yellow-600 buttonWrapper">
+        <button
+          type="button"
+          class="bg-yellow-500 hover:bg-yellow-600 buttonWrapper"
+        >
           <font-awesome-icon icon="fa-solid fa-dolly" class="buttonIcon" />
         </button>
-        <button type="button" class="bg-orange-500 hover:bg-orange-600 buttonWrapper">
+        <button
+          type="button"
+          class="bg-orange-500 hover:bg-orange-600 buttonWrapper"
+        >
           <font-awesome-icon icon="fa-solid fa-percent" class="buttonIcon" />
         </button>
       </div>
@@ -49,6 +80,6 @@
   @apply h-[5.5rem] w-[5.5rem] aspect-square self-center;
 }
 .buttonWrapper {
-  @apply h-full w-full rounded-lg text-white text-6xl flex justify-center
+  @apply h-full w-full rounded-lg text-white text-6xl flex justify-center;
 }
 </style>
--- src/components/search.vue
+++ src/components/search.vue
@@ -1,15 +1,21 @@
 <script setup>
-defineProps(['modelValue'])
-defineEmits(['update:modelValue'])
+defineProps(["modelValue"]);
+defineEmits(["update:modelValue"]);
 </script>
 
 <template>
-  <div class="h-full w-full flex justify-center">
-    <div class="h-[calc(100%_-_1rem)] w-[calc(100%_-_1rem)] border-2 border-gray-200 rounded-lg self-center">
-      <input class="h-full w-full rounded-lg text-6xl" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" placeholder="Enter EAN or Search Term"/>
+  <div class="flex justify-center w-full h-full">
+    <div
+      class="h-[calc(100%_-_1rem)] w-[calc(100%_-_1rem)] border-2 border-gray-200 rounded-lg self-center"
+    >
+      <input
+        class="w-full h-full text-6xl rounded-lg"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        placeholder="Enter EAN or Search Term"
+      />
     </div>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
